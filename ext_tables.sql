2c41722ce8fbc8b6de90a99f9090358517ce40e7
Matthias Gugel <mail@matthias-gugel.de>
2013-10-18 19:13:23 +0200

    [BUGFIX] Add missing tx_squad_role_member_mm table
    Adds the missing create table statement to ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -203,3 +203,16 @@ CREATE TABLE tx_squad_domain_model_memberquestion (
  KEY language (l10n_parent,sys_language_uid)
 
 );
+
+#
+# Table structure for table 'tx_squad_role_member_mm'
+#
+CREATE TABLE tx_squad_role_member_mm (
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
+);
