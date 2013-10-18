8ee7176dd0fc4788c5a12cfb1c5658399acc4444
Matthias Gugel <mail@matthias-gugel.de>
2013-10-18 17:35:24 +0200

    [FEATURE] Add member controller

---
+++ Classes/Controller/MemberController.php
@@ -0,0 +1,65 @@
+<?php
+namespace MFG\Squad\Controller;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Matthias Gugel <mail@matthias-gugel.de>
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ *
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class MemberController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
+	/**
+	 * memberRepository
+	 *
+	 * @var \MFG\Squad\Domain\Repository\MemberRepository
+	 * @inject
+	 */
+	protected $memberRepository;
+
+	/**
+	 * action list
+	 *
+	 * @return void
+	 */
+	public function listAction() {
+		$members = $this->memberRepository->findAll();
+		$this->view->assign('members', $members);
+	}
+
+	/**
+	 * action show
+	 *
+	 * @param \MFG\Squad\Domain\Model\Member $member
+	 * @return void
+	 */
+	public function showAction(\MFG\Squad\Domain\Model\Member $member) {
+		$this->view->assign('member', $member);
+	}
+
+}
+?>
