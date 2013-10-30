08aac1689b007136d8b9fd7d5c128f68fab938d8
Matthias Gugel <mail@matthias-gugel.de>
2013-10-30 03:46:07 +0100

    [FEATURE] Add image upload for member to backend module
    Add upload field to the new member form.
    Process uploaded file in MemberController::createAction.
    Add entries to sys_file and sys_file_reference (more or less by hand).

--- Classes/Controller/MemberController.php
+++ Classes/Controller/MemberController.php
@@ -83,6 +83,15 @@ class MemberController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
 	}
 
 	/**
+	 * initialize create action
+	 */
+	public function initializeAction() {
+		if ($this->arguments->hasArgument('newMember')) {
+			$this->arguments->getArgument('newMember')->getPropertyMappingConfiguration()->setTargetTypeForSubProperty('image', 'array');
+		}
+	}
+
+	/**
 	 * action create
 	 *
 	 * @param \MFG\Squad\Domain\Model\Member $newMember
@@ -100,7 +109,58 @@ class MemberController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
 			}
 		}
 
+		$imageName = $newMember->getImage()['name'];
+		$imageType = $newMember->getImage()['type'];
+		$imageTmpName = $newMember->getImage()['tmp_name'];
+		$imageError = $newMember->getImage()['error'];
+		$imageSize = $newMember->getImage()['size'];
+
+		$storageRepository = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Resource\StorageRepository');
+		$storage = $storageRepository->findByUid(1);
+		$fileObject = $storage->addFile($imageTmpName, $storage->getRootLevelFolder(), $imageName);
+
+		$fileObjectIdentifier = $fileObject->getIdentifier();
+
+		$newMember->setImage(basename($fileObjectIdentifier));
+
 		$this->memberRepository->add($newMember);
+
+		$persistenceManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager');
+		$persistenceManager->persistAll();
+
+		$newMemberUid = $newMember->getUid();
+
+		$newSysFields = array(
+			'pid' => 0,
+			'identifier' => $fileObjectIdentifier,
+			'mime_type' => $imageType,
+			'name' => $fileObjectIdentifier,
+			'size' => $imageSize,
+			'storage' => 1,
+		);
+
+		$newSysRes = $GLOBALS['TYPO3_DB']->exec_INSERTquery('sys_file', $newSysFields);
+		$uid_local = $GLOBALS['TYPO3_DB']->sql_insert_id($newSysRes);
+
+		$data = array();
+		$data['sys_file_reference'][$newMember->getImage()] = array(
+			'uid_local' => $uid_local,
+			'uid_foreign' => $newMemberUid,
+			'tablenames' => 'tx_squad_domain_model_member',
+			'fieldname' => 'image',
+			'pid' => 69, // parent id of the parent page <-- TODO: Remove constant value!
+			'table_local' => 'sys_file',
+			'crdate' => $GLOBALS['EXEC_TIME'],
+			'tstamp' => $GLOBALS['EXEC_TIME']
+		);
+
+		$tce = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\DataHandling\DataHandler'); // create TCE instance
+		$tce->start($data, array(), $new_BE_USER);
+		$tce->process_datamap();
+		/*if ($tce->errorLog) $content .= 'TCE->errorLog:' . t3lib_utility_Debug::viewArray($tce->errorLog);
+		else $content .= 'image changed <br>' . t3lib_utility_Debug::viewArray($data);
+		*/
+
 		$this->flashMessageContainer->add('Your new Member was created.');
 		$this->redirect('list');
 	}
